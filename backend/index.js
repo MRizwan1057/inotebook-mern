76f4110ad957eb7ee6d957dd72255ba6bac8e9ef
Muhammad Rizwan <96943208+MRizwan1057@users.noreply.github.com>
2023-12-15 00:44:44 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+// const __dirname = path.resolve();
 
 const noteRouter = require("./routes/notes");
 const authRouter = require("./routes/auth");
 var cors = require("cors");
 const app = express();
+
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // db connection
 const connectToMongo = () => {
@@ -23,6 +26,16 @@ connectToMongo();
 app.use("/api/auth", authRouter);
 app.use("/api/notes", noteRouter);
 
-app.listen(port, () => {
-  console.log(`chal gya bro ${port} pe`);
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (error) {
+      res.status(500).send(error);
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`chal gya bro ${PORT} pe`);
 });
